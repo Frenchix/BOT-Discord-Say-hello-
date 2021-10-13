c95de6c0197a786f03553fb16d54d831d9012c7e
Frenchix <bapf62@hotmail.com>
2021-10-13 22:45:09 +0200

    ok

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
-    console.log(client.channels.cache.get('609883567899672592').members);
+    // console.log(client.channels.cache.get('609883567899672592').members);
+    const guild = client.guilds.cache.get("239827251540131851");
+    const guildNames = guild.channels.cache.map(g => {
+        console.log(g.name)});
 });
 
-client.on('guildMemberAdd', member => {
-    // This function is executed when a user joins a server
-    // that has your bot in it
-    console.log(member);
-    // /!\ Discord.js uses the term "guild", but it just means "server"
-    // Remember to use "guild" instead of "server" in your code!
-    });
+client.on("guildMemberAdd", (member) => {
+    console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+    member.guild.channels.cache.find(c => c.name === "welcome").send(`"${member.user.username}" has joined this server`);
+});
 
+client.on("guildMemberSpeaking", function(member, speaking){
+    console.log(`a guild member starts/stops speaking: ${member.tag}`);
+});
 // Login to Discord with your client's token
 client.login(process.env.token);
