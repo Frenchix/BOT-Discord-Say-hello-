37c12b3d74badf6ca5c4994b4c2f1208edf1dd9a
Frenchix <bapf62@hotmail.com>
2021-10-14 22:23:11 +0200

    push

--- index.js
+++ index.js
@@ -5,24 +5,20 @@ dotenv.config();
 const { Client, Intents } = require('discord.js');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
     // console.log(client.channels.cache.get('609883567899672592').members);
     const guild = client.guilds.cache.get("239827251540131851");
-    const guildNames = guild.channels.cache.map(g => {
-        console.log(g.name)});
+    // const guildNames = guild.channels.cache.map(g => {
+    //     console.log(g.name)});
+});
+client.on('voiceStateUpdate', function (oldMember, newMember) {
+	// console.log("oldMember", oldMember);
+    console.log("newMember", oldMember.voiceChannel);
 });
 
-client.on("guildMemberAdd", (member) => {
-    console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-    member.guild.channels.cache.find(c => c.name === "welcome").send(`"${member.user.username}" has joined this server`);
-});
-
-client.on("guildMemberSpeaking", function(member, speaking){
-    console.log(`a guild member starts/stops speaking: ${member.tag}`);
-});
 // Login to Discord with your client's token
 client.login(process.env.token);
